3fca114147bf927850fde8900fc18bf991e783c9
Donattai <donattai1032@gmail.com>
2020-10-09 20:40:37 +0100

    modified the app.js file

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', async (req, res) => {
         const shortUrls = await ShortUrl.find({})
         res.render('index', {shortUrls: shortUrls})
     } catch (error) {
-        res.status(404).json({message: error.message})
+        res.sendStatus(404)
     }
    
 })
@@ -37,26 +37,27 @@ app.post('/shorturls', async (req, res) => {
 
     try {
         const shorturl = new ShortUrl(req.body)
-    const result = await shorturl.save()
-    res.redirect('/')
+        const result = await shorturl.save()
+        res.redirect('/')
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.sendStatus(500)
     }
     
 })
 app.get('/:shortUrl', async (req, res) => {
     try {
         const short = req.params.shortUrl
-    const shortUrl = await ShortUrl.findOne({short: short})
-    if(shortUrl === null) {
-        res.status(404).json({message: error.message})
+        const shortUrl = await ShortUrl.findOne({short: short})
+        if(shortUrl === null) {
+        return res.sendStatus(404)
     }
-    shortUrl.save()
-    res.redirect(shortUrl.url) 
+        shortUrl.save()
+        res.redirect(shortUrl.url) 
     } catch (error) {
-        res.status(404).json({message: error.message})
+        res.sendStatus(404)
     }
     
 })
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`)) 
